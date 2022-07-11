d671cc48d82b732ae7fd414ef9e089d4e3cd20a2
noam945 <axelbendavid@gmail.com>
2022-07-11 09:10:19 +0300

    model-selection +avg_dose_response+GPS

--- constants.py
+++ constants.py
@@ -2,3 +2,7 @@ x_columns = ['age', 'sex', 'race', 'ethnicity', 'bmi', 'smoking_status']
 y_column = ['outcome']
 event_columns = ['event_name', 'event_start', 'event_duration']
 
+metadata_columns = ['age', 'sex', 'race', 'ethnicity', 'bmi', 'smoking_status']
+outcome_column = ['outcome']
+treatment_column = ['CA_relative_3']
+
